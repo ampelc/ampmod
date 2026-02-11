47084f5e6ef4fb02dddd122ac57ce4f4a4284e8d
8to16 <ampelectrecuted@gmail.com>
2026-02-11 18:04:44 +0000

    fix(gui): fix paint editor Did so much useless debugging for this, turns out it was just a version mismatch. o_o

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -64,7 +64,6 @@ const CACHE_EPOCH = `amp-${monorepoPackageJson.version}`;
 const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devtool: process.env.SOURCEMAP || (process.env.NODE_ENV === 'production' ? false : 'source-map'),
-    cache: { type: 'filesystem' },
     devServer: {
         static: { directory: path.resolve(__dirname, "build") },
         host: "0.0.0.0",
