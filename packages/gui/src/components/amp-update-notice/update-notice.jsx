b62cccb57da3c0df5363199350d4f19376f0700d
amp <ampelectrecuted@gmail.com>
2026-04-12 15:36:48 +0100

    feat(gui): patchnotes for 0.5

--- packages/gui/src/components/amp-update-notice/update-notice.jsx
+++ packages/gui/src/components/amp-update-notice/update-notice.jsx
@@ -26,8 +26,13 @@ export default props => <Modal
   id="ampmodUpdated"
 >
   <Box className={styles.updateModalContent}>
-    <p>Not yet done</p>
+    <p>Welcome to the fifth update to AmpMod! This will probably be one of the last updates before the release of the project sharing site.</p>
+    <h2>New: Expandable blocks!</h2>
+    <p>The "empty array" block has been replaced with a new block you can expand to add your own items. This makes it easier to create arrays without the delimiter block (which is only good at storing strings).</p>
+    <p>Try using the new + and - buttons on the empty array block.</p>
 
+    <h2>New: Dark theme!</h2>
+    <p>The dark theme has been redesigned to be more modern (but unfortunately less fun). We don't want to eradicate relics from the old era though, so the old one has been renamed to "Dark Colourful".</p>
     <Box className={styles.modalBody}>
       <button className={styles.button} onClick={props.onCancel}>
         Close
